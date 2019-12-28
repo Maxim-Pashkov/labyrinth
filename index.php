7dc37affa5a7ffda98da676764fbefe887179d23
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2019-12-28 20:17:18 +0400

    init input and output files

--- index.php
+++ index.php
@@ -10,9 +10,39 @@ if(!file_exists($argv[1])) {
     die();
 }
 
-$handle = fopen($argv[1], 'r');
-$content = fread($handle, filesize($argv[1]));
-fclose($handle);
+$handleInput = fopen($argv[1], 'r');
+$countTests = (int) fgets($handleInput);
+$result = '';
+if(!$countTests) {
+    echo "Первая строка должна обозначать количество выполняемых тестов\n";
+    die();
+}
 
-echo $content;
+for($i = 0; $i < $countTests; $i++) {
+    $num = $i + 1;
+    $title = "Case #{$num}";
+    $result.= "$title\n";
+
+    $stepsTrack = fgets($handleInput);
+
+    if(!is_string($stepsTrack)) {
+        echo "$title: Не удалось получить строку для задания";
+        die();
+    }
+
+    if(!preg_match("/^W[WLR]*W\sW[WLR]*W$/", $stepsTrack)) {
+        echo "$title: Все пути должны состоять как минимум из двух символов, содержать только символы 'W', 'L', 'R', начинаться и заканчиваться символом 'W'";
+        die();
+    }
+
+    $result.= $stepsTrack;
+
+    $result.= "\n";
+}
+fclose($handleInput);
+
+$handleOutput = fopen("output.txt", 'w');
+fwrite($handleOutput, $result);
+fclose($handleOutput);
+
 die();
